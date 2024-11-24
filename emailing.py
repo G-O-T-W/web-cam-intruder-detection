6018dec88afcf7bed7b01807b33a080942c2fa93
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-11-24 22:35:24 +0530

    Implement sending email by using EmailMessage

--- emailing.py
+++ emailing.py
@@ -1,4 +1,26 @@
-import time
+import smtplib
+from email.message import EmailMessage
 
-def send_email():
-    print("Email sent successfully!")
+PASSWORD = "onboooljmprmrzns"
+SENDER = "rishavdiyali@gmail.com"
+RECEIVER = "diyali.rishav.22@gmail.com"
+
+def send_email(image_path):
+    message = EmailMessage()
+    message["Subject"] = "Intruder Detected!"
+    message.set_content("Hey, your webcam just detected an object.")
+
+    with open(image_path, "rb") as image:
+        image_content = image.read()
+
+    message.add_attachment(image_content, maintype="image", subtype="jpg", filename=image_path)
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, message.as_string())
+    gmail.quit()
+
+if __name__ == "__main__":
+    send_email("images/4.jpg")
