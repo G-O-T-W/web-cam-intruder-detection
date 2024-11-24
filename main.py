825328cd3acd64d7347ed48baf07361ef287c1f8
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-11-24 22:37:22 +0530

    Fix Screen Freeze when object detected

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import cv2
 import time
 from emailing import send_email
+import glob
+import os
+from threading import Thread
+
+
+def clean_folder():
+    print("starting clean_folder()")
+    images = glob.glob('*.jpg')
+    for image in images:
+        os.remove(image)
 
 """
     To use system webcam use:
     video = cv2.VideoCapture(0)
 """
-video = cv2.VideoCapture(2)
+video = cv2.VideoCapture(0)
 time.sleep(1)
 first_frame = None
 # status_list is used to detect object entry and exit
 status_list = []
 
+img_count = 0
+
+clean_folder()
 while True:
     status = 0
     check, frame = video.read()
@@ -23,27 +36,37 @@ while True:
         first_frame = grayscale_gau
 
     delta_frame = cv2.absdiff(first_frame, grayscale_gau)
-    thresh_frame = cv2.threshold(delta_frame, 70, 255, cv2.THRESH_BINARY)[1]
-    dilate_frame = cv2.dilate(thresh_frame, None, iterations=3)
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dilate_frame = cv2.dilate(thresh_frame, None, iterations=5)
 
+    # cv2.imshow("Video", dilate_frame)
+    # print(delta_frame)
 
     contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 5000:
+        if cv2.contourArea(contour) < 6000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{img_count}.jpg", frame)
+            img_count = img_count + 1
+            all_images = glob.glob("images/*.jpg")
+            img_idx = int(len(all_images) / 2)
+            img_with_object = all_images[img_idx]
+
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
-
-
+        email_thread = Thread(target=send_email, args=(img_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+        email_thread.start()
 
     cv2.imshow('Video', frame)
 
@@ -51,3 +74,5 @@ while True:
     if key == ord('q'):
         break
 
+clean_thread.start()
+video.release()
